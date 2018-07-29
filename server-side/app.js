3ccf31cbb3d32ae9a113f29744eb0c1a8b820bd6
Vince Falconi <vince.falconi@gmail.com>
2018-07-29 00:14:21 -0500

    update server-side app to use better method params

--- server-side/app.js
+++ server-side/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const SERVER_PORT = 3333;
 const app = express();
 const fs = require('fs');
 const handlebars = require('handlebars');
@@ -24,26 +25,26 @@ fs.readFile('./results.html', 'utf-8', (error, source) => {
 			isPounds: (inputUnit === 'lbs'),
 			isStones: (inputUnit === 'st'),
 			iwf: {
-				'men': weightClassData.get(convertedWeight, 'iwf', 'men'),
-				'women': weightClassData.get(convertedWeight, 'iwf', 'women'),
-				'youth_men': weightClassData.get(convertedWeight, 'iwf', 'youth_men'),
-				'youth_women': weightClassData.get(convertedWeight, 'iwf','youth_women' )
+				'men': weightClassData.get(convertedWeight, weightClassData.iwf.men),
+				'women': weightClassData.get(convertedWeight, weightClassData.iwf.women),
+				'youth_men': weightClassData.get(convertedWeight, weightClassData.iwf.youth_men),
+				'youth_women': weightClassData.get(convertedWeight, weightClassData.iwf.youth_women)
 			},
 			ipl: {
-				'men': weightClassData.get(convertedWeight, 'ipl', 'men'),
-				'women': weightClassData.get(convertedWeight, 'ipl', 'women')
+				'men': weightClassData.get(convertedWeight, weightClassData.ipl.men),
+				'women': weightClassData.get(convertedWeight, weightClassData.ipl.women)
 			},
 			ipf: {
-				'men': weightClassData.get(convertedWeight, 'ipf', 'men'),
-				'women': weightClassData.get(convertedWeight, 'ipf', 'women')
+				'men': weightClassData.get(convertedWeight, weightClassData.ipf.men),
+				'women': weightClassData.get(convertedWeight, weightClassData.ipf.women)
 			},
 			us_strongman: {
-				'open_strongman': weightClassData.get(convertedWeight, 'us_strongman', 'open_strongman'),
-				'masters_strongman': weightClassData.get(convertedWeight, 'us_strongman', 'masters_strongman'),
-				'teens_strongman': weightClassData.get(convertedWeight, 'us_strongman', 'teens_strongman'),
-				'open_strongwoman': weightClassData.get(convertedWeight, 'us_strongman', 'open_strongwoman'),
-				'masters_strongwoman': weightClassData.get(convertedWeight, 'us_strongman', 'masters_strongwoman'),
-				'teens_strongwoman': weightClassData.get(convertedWeight, 'us_strongman', 'teens_strongwoman')
+				'open_strongman': weightClassData.get(convertedWeight, weightClassData.us_strongman.open_strongman),
+				'masters_strongman': weightClassData.get(convertedWeight, weightClassData.us_strongman.masters_strongman),
+				'teens_strongman': weightClassData.get(convertedWeight, weightClassData.us_strongman.teens_strongman),
+				'open_strongwoman': weightClassData.get(convertedWeight, weightClassData.us_strongman.open_strongwoman),
+				'masters_strongwoman': weightClassData.get(convertedWeight, weightClassData.us_strongman.masters_strongwoman),
+				'teens_strongwoman': weightClassData.get(convertedWeight, weightClassData.us_strongman.teens_strongwoman)
 			}
 		};
 		const output = template(templateData);
@@ -51,5 +52,5 @@ fs.readFile('./results.html', 'utf-8', (error, source) => {
 		res.send(output);
 	});
 
-	app.listen(3000, () => console.log('Example app listening on port 3000!'));
+	app.listen(SERVER_PORT, () => console.log(`Example app listening on port ${SERVER_PORT}!`));
 });
